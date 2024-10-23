0ad1f8e66e666155c3c597b48a4186c6010a54ed
chevaliernumeric <mamadoulamine.bousso@uvs.edu.sn>
2024-10-23 18:41:31 +0200

    five commit

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\adminController;
-use App\Http\Controllers\casierController;
-use App\Http\Controllers\clientController;
-use App\Http\Controllers\loginController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CasierController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,43 +21,43 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 //login controller
-Route::get('/', [loginController::class, 'login']);
-Route::post('/client/access',[loginController::class,'access']);
-Route::get('/client/logout',[loginController::class,'logout']);
-Route::post('/casier/loginaccess',[loginController::class,'loginaccess']);
-Route::get('/casier/logoutc',[loginController::class,'logoutc']);
+Route::get('/', [LoginController::class, 'login']);
+Route::post('/client/access',[LoginController::class,'access']);
+Route::get('/client/logout',[LoginController::class,'logout']);
+Route::post('/casier/loginaccess',[LoginController::class,'loginaccess']);
+Route::get('/casier/logoutc',[LoginController::class,'logoutc']);
 
 
 
 //admin controller
-Route::get('admin/home', [adminController::class, 'home']);
-Route::get('admin/account', [adminController::class, 'account']);
-Route::get('admin/addaccount', [adminController::class, 'addaccount']);
-Route::get('admin/feedback', [adminController::class, 'feedback']);
-Route::get('admin/clientdetail/{id}', [adminController::class, 'clientdetail']);
-Route::get('admin/sendnotice/{id}', [adminController::class, 'sendnotice']);
-Route::post('admin/addcaisier', [adminController::class, 'addcaisier']);
-Route::post('admin/updatecasier', [adminController::class, 'updatecasier']);
-Route::get('admin/deletecasier/{id}', [adminController::class, 'deletecasier']);
-Route::post('admin/saveaccount', [adminController::class, 'saveaccount']);
-Route::get('admin/deleteclient/{id}',[adminController::class,'deleteclient']);
-Route::post('admin/notice',[adminController::class,'notice']);
-Route::get('admin/deletefeed/{id}',[adminController::class,'deletefeed']);
+Route::get('admin/home', [AdminController::class, 'home']);
+Route::get('admin/account', [AdminController::class, 'account']);
+Route::get('admin/addaccount', [AdminController::class, 'addaccount']);
+Route::get('admin/feedback', [AdminController::class, 'feedback']);
+Route::get('admin/clientdetail/{id}', [AdminController::class, 'clientdetail']);
+Route::get('admin/sendnotice/{id}', [AdminController::class, 'sendnotice']);
+Route::post('admin/addcaisier', [AdminController::class, 'addcaisier']);
+Route::post('admin/updatecasier', [AdminController::class, 'updatecasier']);
+Route::get('admin/deletecasier/{id}', [AdminController::class, 'deletecasier']);
+Route::post('admin/saveaccount', [AdminController::class, 'saveaccount']);
+Route::get('admin/deleteclient/{id}',[AdminController::class,'deleteclient']);
+Route::post('admin/notice',[AdminController::class,'notice']);
+Route::get('admin/deletefeed/{id}',[AdminController::class,'deletefeed']);
 
 //client controller
 
-Route::get('client/home', [clientController::class, 'home']);
-Route::get('client/account', [clientController::class, 'account']);
-Route::get('client/statement', [clientController::class, 'statement']);
-Route::get('client/fund', [clientController::class, 'fund']);
-Route::get('client/feedback', [clientController::class, 'feedback']);
-Route::get('client/notice', [clientController::class, 'notice']);
-Route::post('client/clientmessage',[clientController::class,'clientmessage']);
-Route::post('client/clienttransfer',[clientController::class,'clienttransfer']);
-Route::post('client/transfer',[clientController::class,'transfer']);
+Route::get('client/home', [ClientController::class, 'home']);
+Route::get('client/account', [ClientController::class, 'account']);
+Route::get('client/statement', [ClientController::class, 'statement']);
+Route::get('client/fund', [ClientController::class, 'fund']);
+Route::get('client/feedback', [ClientController::class, 'feedback']);
+Route::get('client/notice', [ClientController::class, 'notice']);
+Route::post('client/clientmessage',[ClientController::class,'clientmessage']);
+Route::post('client/clienttransfer',[ClientController::class,'clienttransfer']);
+Route::post('client/transfer',[ClientController::class,'transfer']);
 //casier controller
 
-Route::get('casier/home', [casierController::class, 'home']);
-Route::post('casier/casiertransfer',[casierController::class,'casiertransfer']);
-Route::post('casier/withdraw',[casierController::class,'withdraw']);
-Route::post('casier/deposit',[casierController::class,'deposit']);
+Route::get('casier/home', [CasierController::class, 'home']);
+Route::post('casier/casiertransfer',[CasierController::class,'casiertransfer']);
+Route::post('casier/withdraw',[CasierController::class,'withdraw']);
+Route::post('casier/deposit',[CasierController::class,'deposit']);
